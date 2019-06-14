46632dd7e67ad5358dae07b6a0bd89e442dc4d8f
ghkdtnsejr6 <51368335+ghkdtnsejr6@users.noreply.github.com>
2019-06-15 07:20:34 +0900

    Update generate-seeds.py

--- share/seeds/generate-seeds.py
+++ share/seeds/generate-seeds.py
@@ -130,7 +130,7 @@ def main():
         process_nodes(g, f, 'pnSeed6_main', 1993)
     g.write('\n')
     with open(os.path.join(indir,'nodes_test.txt'),'r') as f:
-        process_nodes(g, f, 'pnSeed6_test', 11993)
+        process_nodes(g, f, 'pnSeed6_test', 19931)
     g.write('#endif // DASH_CHAINPARAMSSEEDS_H\n')
             
 if __name__ == '__main__':
